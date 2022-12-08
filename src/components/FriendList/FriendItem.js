2fb99f0f88fb1a07fd2717cd0537802a6eadd939
Hurduz Oksana <hurduzoksana@gmail.com>
2022-12-08 23:43:59 +0200

    adding isOnline

--- src/components/FriendList/FriendItem.js
+++ src/components/FriendList/FriendItem.js
@@ -5,7 +5,7 @@ export default function FriendItem({ friends }) {
     <div>
         {friends.map(el => (
         <li className={css.item} key={el.id}>
-  <span className={css.status}></span>
+  <span className={el.isOnline ? css.online : css.offline}></span>
   <img className={css.avatar} src={el.avatar} alt="User avatar" width="48" />
   <p className={css.name}>{el.name}</p>
   </li>
